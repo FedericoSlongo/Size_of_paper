73e232e61aab39a1cc13a720758dcee2d3e4744c
Federico Slongo <73640124+FedericoSlongo@users.noreply.github.com>
2021-10-26 16:18:18 +0200

    Update Fogli AN.cs

--- Fogli AN/Fogli AN.cs
+++ Fogli AN/Fogli AN.cs
@@ -6,19 +6,19 @@ namespace Fogli_AN
     {
         static void Main(string[] args)
         {
-                double A0y = 1189, A0x = 841, temp = 0, temp2 = 0, temp3 = 0;
-                Console.Write("Inserire la dimensione di A");
-                temp = Convert.ToDouble(Console.ReadLine());
-                switch (temp)
-                {
-                    case 0:
-                        Console.WriteLine($"A{temp} {A0y} mm * {A0x} mm");
-                        break;
-                    default:
-                        temp2 = Math.Pow(1.4142, temp);
-                        Console.Write($"A{temp} {A0y / temp2} mm * {A0x / temp2} mm");
-                        break;
-                }
+            double A0y = 1189, A0x = 841, A = 0, temp2 = 0;
+            Console.Write("Inserire la dimensione di A");
+            A = Convert.ToDouble(Console.ReadLine());
+            switch (A)
+            {
+                case 0:
+                    Console.WriteLine($"A{A} {A0y} mm * {A0x} mm");
+                    break;
+                default:
+                    temp2 = Math.Pow(1.4142, A);
+                    Console.WriteLine($"A{A} {(A0y / temp2):000.00} mm * {(A0x / temp2):000.00} mm");
+                    break;
+            }
         }
     }
 }
